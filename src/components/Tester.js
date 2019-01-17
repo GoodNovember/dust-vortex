312bdfbbb62a163cdfa907705422b4f989d56881
Victor Lawrence <VictorLawrenceCode@gmail.com>
2019-01-17 17:55:51 -0500

    Made things a little more stylish with styled-components

--- src/components/Tester.js
+++ src/components/Tester.js
@@ -1,12 +1,38 @@
 import React from 'react'
+import styled from 'styled-components'
+
+const Hugger = styled.div`
+  border: 1px solid black;
+  background: pink
+  padding: 1rem;
+`
+
+const Description = styled.div`
+  border: 1px solid black;
+  background: yellow;
+  padding: 1rem;
+`
+
+const Title = styled.div`
+  border: 1px sold black;
+  background: teal;
+  padding: 1rem;
+  color: white;
+`
+
+const IdElm = styled.div`
+  border: 1px solid black;
+  background: azure;
+  color: red;
+  padding: 1rem;
+`
 
 const Tester = ({ id, description, title }) => (
-  <div>
-    <div>[Tester Element]</div>
-    <div>ID:{ id }</div>
-    <div>Title:{ title }</div>
-    <div>Description:{ description }</div>
-  </div>
+  <Hugger>
+    <IdElm>{ id }</IdElm>
+    <Title>{ title }</Title>
+    <Description>{ description }</Description>
+  </Hugger>
 )
 
 export default Tester
