08d4f666d9a0cf086b58ecbc94780e60e095abe1
Victor Lawrence <VictorLawrenceCode@gmail.com>
2019-01-17 17:35:21 -0500

    Removed something unused

--- src/templates/blog-post.js
+++ src/templates/blog-post.js
@@ -4,7 +4,7 @@ import { kebabCase } from 'lodash'
 import Helmet from 'react-helmet'
 import { graphql, Link } from 'gatsby'
 import Layout from '../components/Layout'
-import Content, { HTMLContent } from '../components/Content'
+import Content from '../components/Content'
 
 import RehypeReact from 'rehype-react'
 import Tester from '../components/Tester'
